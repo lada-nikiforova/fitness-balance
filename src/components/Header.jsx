7d048bb8f66c4dfeaf71401c927803fe4aa075a7
lada-nikiforova <ladanik2002@gmail.com>
2025-05-14 16:45:29 +0300

    change logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import logo from '../icons/logo.svg';
+import logo from '../icons/logo3.png';
 import { useState } from 'react';
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     return (
         <header className="fixed top-0 left-0 w-full bg-header shadow-md z-50">
-            <nav className=' px-10 py-4 md:py-5 lg:px-15 lg:py-5'>
+            <nav className=' px-10 py-2 md:py-5 lg:px-15 lg:py-5'>
                 <div className='flex justify-between'>
                     <a href="#">
                         <img className='w-27 md:w-30 lg:w-40' src={logo} alt="Logo"/>
@@ -12,12 +12,12 @@ const Header = () => {
                     <button className={`lg:hidden w-10 z-52 cursor-pointer relative flex flex-col justify-center items-center`} onClick={() => setMenuOpen(!menuOpen)}>
                         <span
                             className={`absolute rounded left-0 h-0.5 bg-pink transition-transform duration-300 ease-in-out ${
-                            menuOpen ? 'w-7 md:w-8 transform rotate-45 top-3.5' : 'w-6  rotate-0 top-1'
+                            menuOpen ? 'w-7 md:w-8 transform rotate-45 top-4' : 'w-6  rotate-0 top-2'
                             }`}
                         />
                         <span
                             className={`absolute rounded left-0 h-0.5 bg-pink transition-transform duration-300 ease-in-out ${
-                            menuOpen ? 'w-7 md:w-8 transform -rotate-45 bottom-2.5' : ' w-9 rotate-0 md:bottom-0.5 bottom-1'
+                            menuOpen ? 'w-7 md:w-8 transform -rotate-45 bottom-3.5' : ' w-9 rotate-0 md:bottom-0.5 bottom-2'
                             }`}
                         />
                     </button>
@@ -29,7 +29,7 @@ const Header = () => {
                         </ul>
                     </nav>
                     {menuOpen && <nav className='lg:hidden fixed top-0 left-0 w-full h-full bg-white z-50'>
-                        <div className='px-10 py-4'>
+                        <div className='px-10 py-2'>
                             <a href="#">
                                 <img className='w-27 md:w-30' src={logo} alt="Logo"/>
                             </a>
