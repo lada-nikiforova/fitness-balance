72bc56bb678937ccbe603bbfbed9cea63a6d71fe
lada-nikiforova <ladanik2002@gmail.com>
2025-06-05 16:04:21 +0300

    fix width card price

--- src/components/CardPrice.jsx
+++ src/components/CardPrice.jsx
@@ -6,7 +6,7 @@ const CardPrice = ({label, price, rules, price1, ref}) => {
       <h1 className="font-bold text-center py-1.5 lg:py-2 bg-pink text-white text-lg md:text-xl lg:text-2xl rounded-t-2xl ">
           {label}
         </h1>
-      <div className='px-4 py-5 w-64 h-60 shadow-sm  bg-white border-3 border-pink rounded-b-2xl flex flex-col justify-between items-center mb-3'>
+      <div className='px-4 py-5 w-65 h-60 shadow-sm  bg-white border-3 border-pink rounded-b-2xl flex flex-col justify-between items-center mb-3'>
         <p className='whitespace-pre-line text-sm mb-4 text-dark'>{rules}</p>
         <div><p className='font-druk tracking-wider text-4xl text-pink'>{price}</p>
         <p className='text-dark text-sm'>{price1}</p></div>
